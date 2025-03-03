346a904e2b236071dccef4873c37cf8db3a21bf7
Sigstus985 <siggeluren@gmail.com>
2025-03-03 11:53:11 +0100

    added submission functionality

--- react-typescript-project/src/App.tsx
+++ react-typescript-project/src/App.tsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import InputField from "./Components/InputField";
+import { Todo } from "./model";
 
 const App: React.FC = () => {
+	const [todo, setTodo] = useState<string>("");
+	const [todos, setTodos] = useState<Todo[]>([]);
+
+	const handleAdd = (e: React.SyntheticEvent) => {
+		e.preventDefault();
+
+		if (todo) {
+			setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+			setTodo("");
+		}
+	};
+
 	return (
 		<div className="App">
-      <h1 className="header">Taskmaker</h1>
+			<h1 className="header">Taskmaker</h1>
 
-      <InputField></InputField>
+			<InputField
+				todo={todo}
+				setTodo={setTodo}
+				handleAdd={handleAdd}
+			></InputField>
+      {/* <TodoList></TodoList> */}
 		</div>
 	);
-}
+};
 
 export default App;
--- react-typescript-project/src/Components/InputField.tsx
+++ react-typescript-project/src/Components/InputField.tsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useRef } from "react";
 
-const InputField = () => {
-    return(
-        <form className="input">
-            <input type="input" placeholder="Create a task" className="input__box" />
-        </form>
-    )
-}
+import "./styles.css";
 
-export default InputField
+type Props = {
+	todo: string;
+	setTodo: React.Dispatch<React.SetStateAction<string>>;
+	handleAdd: (e: React.SyntheticEvent) => void;
+};
+
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
+	const inputRef = useRef<HTMLInputElement>(null);
+	return (
+		<form
+			className="input"
+			onSubmit={(e) => {
+				handleAdd(e);
+				inputRef.current?.blur();
+			}}
+		>
+			<input
+				ref={inputRef}
+				type="input"
+				value={todo}
+				onChange={(e) => setTodo(e.target.value)}
+				placeholder="Create a task"
+				className="input__box"
+			/>
+			<button className="input__submit" type="submit">
+				Go
+			</button>
+		</form>
+	);
+};
+
+export default InputField;
